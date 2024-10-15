0ffba540c29a47146c45fd48524bed2f933e13d5
root <root@m2-maprts-vm207-172.mip.storage.hpecorp.net>
2024-10-15 15:01:08 -0400

    my dags changes

---
+++ dags/sample_http_req.py
@@ -0,0 +1,43 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+from datetime import timedelta
+
+# Default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),  # Retry after 5 minutes if a task fails
+}
+
+# Define the DAG (Directed Acyclic Graph)
+dag = DAG(
+    'simple_dag_example',
+    default_args=default_args,
+    description='A simple DAG with two tasks',
+    schedule_interval=timedelta(days=1),  # Runs every day
+    start_date=days_ago(2),  # Start date is 2 days ago
+    catchup=False,  # Do not backfill if start date is in the past
+)
+
+# Task 1: Print the current date using BashOperator
+print_date = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=dag,
+)
+
+# Task 2: Sleep for 5 seconds using BashOperator
+sleep_task = BashOperator(
+    task_id='sleep_task',
+    bash_command='sleep 5',
+    dag=dag,
+)
+
+# Define task dependencies
+# sleep_task will run after print_date
+print_date >> sleep_task
+
