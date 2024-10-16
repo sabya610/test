70a9d221afaf256f52cdd4ce0a4d680390d3d112
sabya610 <122990341+sabya610@users.noreply.github.com>
2024-10-16 11:52:03 +0530

    Create Dag_mail.py

---
+++ dags/Dag_mail.py
@@ -0,0 +1,68 @@
+import smtplib
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.utils.dates import days_ago
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),  # Retry after 5 minutes if a task fails
+}
+
+dag=DAG(
+     'simple_mail',
+     default_args=default_args,
+     description='Simple task to send mail',
+     schedule_interval='None',
+     catchup=False
+
+)
+
+
+
+def hpe_email():
+ 
+    now = datetime.now() # current date and time
+    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+ 
+    SUBJECT = "SMTP e-mail test"
+    TEXT = f"Hello RK, How are you doing {date_time}"
+ 
+    message = f'Subject: {SUBJECT}\n\n{TEXT}'
+ 
+    print(f"%%%%%%%%% MESSAGE = {message} %%%%%%%%")
+ 
+    sender = 'rakesh.singh2@hpe.com'
+    receivers = 'rksingh61@yahoo.com'
+ 
+    try:
+        smtpObj = smtplib.SMTP('smtp.its.hpecorp.net')
+        smtpObj.sendmail(sender, receivers, message)
+        print("Successfully sent email")
+ 
+    except smtplib.SMTPException:
+        print("Error: unable to send email")
+ 
+    # Close the connection
+    smtpObj.close()
+
+#Task 1 :Print current date using Bash Operator
+print_date=BashOPerator(
+     task_id='Print Date',
+     bash_command='date',
+     dag=dag
+)
+
+#Task 2: Send Mail using smtp and pythonoperator
+mail_send=PythonOperator(
+      task_id='Mail_Sending Task',
+      python_callable=hpe_email,
+      dag=dag
+)
+
+print_date>>mail_send
