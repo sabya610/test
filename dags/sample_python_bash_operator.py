580eef0eb06c31499c44fe6db20b7cfe2769031d
sabya610 <122990341+sabya610@users.noreply.github.com>
2026-02-25 01:41:03 +0530

    included param module

--- dags/sample_python_bash_operator.py
+++ dags/sample_python_bash_operator.py
@@ -3,6 +3,7 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from airflow.operators.email import EmailOperator
 from airflow.utils.dates import days_ago
+from airflow.models.param import Param
 
 
 # Python function for PythonOperator
